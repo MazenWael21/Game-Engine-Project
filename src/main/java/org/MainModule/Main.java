ceca51fe74da865eb68976e7b524af4629336eb0
yaseen <yaseentarek216@gmail.com>
2024-08-27 15:36:42 +0300

    initial commit. -Report: running the build files and all the liberaries are working fine without any problem.
    BUILD SUCCESSFUL in 195ms
    2 actionable tasks: 1 executed, 1 up-to-date
    3:34:57 PM: Execution finished ':org.MainModule.Main.main()'.

---
+++ src/main/java/org/MainModule/Main.java
@@ -0,0 +1,9 @@
+package org.MainModule;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi");
+
+
+    }
+}
---
+++ src/main/java/org/MainModule/Window.java
@@ -0,0 +1,6 @@
+package org.MainModule;
+
+public class Window {
+
+
+}
